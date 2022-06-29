45f3d5e3869a287612c11f62f3fa1c81c46b6059
EC2 Default User <ec2-user@ip-172-31-4-3.ec2.internal>
2022-06-29 03:58:34 +0000

    commit

--- spontaneous_intro.py
+++ spontaneous_intro.py
@@ -1,1 +1,20 @@
+# test print statment
+
 print("Hello World")
+
+# demostrate strings
+Hello = "Hello"
+var_1 = Hello
+
+
+# demostrate boolean
+var_2 = True
+var_3 = False
+
+# demostrate numbers
+var_4 = 2 # int(eger)
+var_5 = 2.06 # float
+
+# demostrate type casting
+print(float(var_4))
+print(int(var_5))
